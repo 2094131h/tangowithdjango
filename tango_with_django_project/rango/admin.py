6b18a2334e56c3a7f9bbaf17675054162f90c33c
Craig Hamill <craighamill1@msn.com>
2015-01-26 22:02:09 +0000

    Chapter 7 - completed

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+<<<<<<< HEAD
 # Add in this class to customized the Admin Interface
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
@@ -8,6 +9,17 @@ class CategoryAdmin(admin.ModelAdmin):
 # Update the registeration to include this customised interface
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
+=======
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+    def __unicode__(self):
+        return self.name
+    
+#Register models
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+>>>>>>> 12659ef... Chapter 4 - completed
 
 
 
