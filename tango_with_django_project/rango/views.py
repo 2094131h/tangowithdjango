27a4f3294a64420f8867ca4652f66dde3a7f3585
Craig Hamill <craighamill1@msn.com>
2015-01-29 13:27:08 +0000

    Chapter 8 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Page
 from rango.models import Category
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+
+
+def add_page(request, category_name_slug):
+
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+                cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form':form, 'category': cat}
+
+    return render(request, 'rango/add_page.html', context_dict)
+
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -14,6 +44,7 @@ def index(request):
 
     # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
+
 
 def about(request):
     context_dict = {'aboutMessage': "Welcome to my page!"}
@@ -29,8 +60,11 @@ def category(request, category_name_slug):
         # Can we find a category name slug with the given name?
         # If we can't, the .get() method raises a DoesNotExist exception.
         # So the .get() method returns one model instance or raises an exception.
+
         category = Category.objects.get(slug=category_name_slug)
+
         context_dict['category_name'] = category.name
+        context_dict['category_slug'] = category.slug
 
         # Retrieve all of the associated pages.
         # Note that filter returns >= 1 model instance.
@@ -48,3 +82,28 @@ def category(request, category_name_slug):
 
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
